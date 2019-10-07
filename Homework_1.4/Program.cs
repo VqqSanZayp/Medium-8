eb41467e2f245ced4547b4cf45f1f53a7f498e6e
San Zayp <SanZayp@Outlook.com>
2019-10-08 05:36:56 +0600

    Homework_1.4

---
+++ Homework_1.4/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_1._4
+{
+    class Player
+{
+    public string Name { get; private set; }
+    public int Age { get; private set; }
+
+    private Movement _movement;
+    private Weapon _weapon;
+
+    public Player(string name, int age, Movement movement, Weapon weapon)
+    {
+        Name = name;
+        Age = age;
+
+        _movement = movement;
+        _weapon = weapon;
+    }
+
+    public void Move() => _movement.Run();
+    public void Attack()
+    {
+        if (_weapon.IsReloading())
+        {
+            _weapon.Attack();
+        }
+    }
+}
+
+    class Weapon
+    {
+        public int Damage { get; private set; }
+        public float Cooldown { get; private set; }
+
+        public void Attack()
+        {
+            //attack
+        }
+
+        public bool IsReloading()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Movement
+    {
+        public float DirectionX { get; private set; }
+        public float DirectionY { get; private set; }
+        public float Speed { get; private set; }
+
+        public void Run()
+        {
+            //Do move
+        }
+    }
+}
