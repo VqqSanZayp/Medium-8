4cbfef3263cc9c8bef16e2c2430e529804673f48
San Zayp <SanZayp@Outlook.com>
2019-10-08 07:17:31 +0600

    Homework_2.1

---
+++ Homework_2.1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_2._1
+{
+    class Unit
+    {
+        public int Health { get; private set; }
+
+        public void TakeDamage(int damage)
+        {
+            Health -= TypeOfDamage(damage);
+            if (Health <= 0)
+            {
+                Console.WriteLine("Я умер");
+            }
+        }
+
+        public virtual int TypeOfDamage(int damage) => damage;
+    }
+
+    class Wombat : Unit
+    {
+        public int Armor { get; private set; }
+
+        public override int TypeOfDamage(int damage) => damage - Armor;
+    }
+
+    class Human : Unit
+    {
+        public int Agility { get; private set; }
+
+        public override int TypeOfDamage(int damage) => damage / Agility;
+    }
+}
